92c2eee96039f49479d60028c5767d7348d91c42
lexjesus <52578529+lexjesus@users.noreply.github.com>
2022-01-03 12:19:24 +0300

    Add files via upload

---
+++ MyPoint.java
@@ -0,0 +1,27 @@
+package com.example.java_test;
+
+public class MyPoint {
+    int x;
+    int y;
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    void setX(int x) {
+        this.x = x;
+    }
+
+    void setY(int y) {
+        this.y = y;
+    }
+}
